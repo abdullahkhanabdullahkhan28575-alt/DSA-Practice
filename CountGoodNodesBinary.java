e1536e94e221f1af1e1dceae4df9f237050922cc
Abdullah Khan <abdullahkhanabdullahkhan28575@gmail.com>
2026-05-23 12:12:57 +0530

    count good  nodes binary code

---
+++ CountGoodNodesBinary.java
@@ -0,0 +1,23 @@
+public class CountGoodNodesBinary {\
+     public int goodNodes(TreeNode root) {
+        return dfs(root, root .val);
+
+
+        
+    }
+    private int dfs(TreeNode node, int maxval){
+        if(node==null){
+            return 0;
+
+        }
+         int res =(node.val >=maxval)?1:0;
+         maxval =Math.max(maxval,node.val);
+         res+=dfs(node.left, maxval);
+         res +=dfs(node .right, maxval);
+         return res;
+
+
+
+    }
+    
+}
