454b7beae09a975418b03958bc184e1777483dcc
Abdullah Khan <abdullahkhanabdullahkhan28575@gmail.com>
2026-07-30 13:40:04 +0530

    Add Unique_Paths Solution

---
+++ Unique_Paths.java
@@ -0,0 +1,16 @@
+public class Unique_Paths {
+     public int uniquePaths(int m, int n) {
+        int [][] arr=new int [m+1][n+1];
+        arr[m-1][n-1]=1;
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                arr[i][j]+=arr[i+1][j]+arr[i][j+1];
+
+            }
+        }
+        return arr[0][0];
+
+        
+    }
+    
+}
