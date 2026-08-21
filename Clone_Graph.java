817e95a652beec091558ce899a375847c05cb2d2
Abdullah Khan <abdullahkhanabdullahkhan28575@gmail.com>
2026-08-21 12:28:09 +0530

    Add Clone_Graph

---
+++ Clone_Graph.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+
+
+
+
+
+
+
+   
+
+ class Clone_Graph {
+    class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        Map<Node, Node> oldToNew = new HashMap<>();
+        Queue<Node> q = new LinkedList<>();
+        oldToNew.put(node, new Node(node.val));
+        q.add(node);
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+            for (Node nei : cur.neighbors) {
+                if (!oldToNew.containsKey(nei)) {
+                    oldToNew.put(nei, new Node(nei.val));
+                    q.add(nei);
+                }
+                oldToNew.get(cur).neighbors.add(oldToNew.get(nei));
+            }
+        }
+        return oldToNew.get(node);
+    }
+}
+    
+
