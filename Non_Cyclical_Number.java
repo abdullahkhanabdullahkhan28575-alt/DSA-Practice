98330e833187b130596bbe068f8d60ce0004f654
Abdullah Khan <abdullahkhanabdullahkhan28575@gmail.com>
2026-08-24 17:21:00 +0530

    Add Non_Cyclical_Number Solution

---
+++ Non_Cyclical_Number.java
@@ -0,0 +1,24 @@
+public class Non_Cyclical_Number {
+       public boolean isHappy(int n) {
+        int slow=n, fast=sumOfSquares(n);
+        while(slow !=fast){
+            fast =sumOfSquares(fast);
+            fast =sumOfSquares(fast);
+            slow =sumOfSquares(slow);
+
+        }
+        return  fast==1;
+        
+    }
+    private int sumOfSquares(int n){
+        int output=0;
+        while(n!=0){
+            output +=(n % 10)*(n % 10);
+            n/=10;
+
+        }
+        return output;
+
+    }
+    
+}
