4dcec833eb1aaff3f7e058ec2801fdc716e507c1
Abdullah Khan <abdullahkhanabdullahkhan28575@gmail.com>
2026-09-15 20:02:36 +0530

    Add Reverse_LinkedList

---
+++ Reverse_LinkedList.java
@@ -0,0 +1,19 @@
+public class Reverse_LinkedList {
+     public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=head;
+        
+        while(curr!=null){
+         ListNode   next=curr.next;
+            curr.next=prev;
+
+
+
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+        
+    }
+    
+}
